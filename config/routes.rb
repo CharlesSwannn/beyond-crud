c739f4506d931c80eb475e01e34c5bb87a5f360d
André Marques <115092696+CharlesSwannn@users.noreply.github.com>
2023-11-15 12:25:46 +0100

    added reviews controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,6 @@
 Rails.application.routes.draw do
   resources :restaurants do
-    collection do
-      get :top
-    end
-    member do
-      get :chef
-    end
-    
-
+    resources :reviews, only: [:new, :index, :create]
   end
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :reviews, only: [:destroy]
 end
